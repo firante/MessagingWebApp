86adc23d9004fd05e7ebe71c1ae9e194270b43c2
Misha Fedorovych <misha.fedorovych@gmail.com>
2016-04-19 09:10:33 +0300

    'App2'

--- MessagingWebApp/client/main.jsx
+++ MessagingWebApp/client/main.jsx
@@ -2,18 +2,10 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
-import App from '../lib/ui/App.jsx';
+import App from '../imports/ui/App.jsx';
 
 Meteor.startup(() => {
-	
-	render(<App />, document.getElementById('container'));
 
-
+	render (<App />, document.getElementById('container'));
 
 });
-
-while(Meteor.user() == 'undefined') {
-		console.log(Meteor.user());
-	}
-	console.log(Meteor.user());
-
--- MessagingWebApp/imports/ui/App.jsx
+++ MessagingWebApp/imports/ui/App.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import AccountUIWrapper from './AccountsUIWrapper.jsx';
 import { render } from 'react-dom';
 
+import '../startup/accounts-config.js';
+
 export default class App extends Component {
 	render() {
 		return(
