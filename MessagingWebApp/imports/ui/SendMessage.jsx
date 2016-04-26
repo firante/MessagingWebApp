0a4a7928d76fdaf5cc7f2ce1d55220ce22e2e6ce
Misha Fedorovych <misha.fedorovych@gmail.com>
2016-04-26 08:35:57 +0300

    'www'

--- MessagingWebApp/imports/ui/SendMessage.jsx
+++ MessagingWebApp/imports/ui/SendMessage.jsx
@@ -1,13 +1,25 @@
 import React, {Component, PropTypes} from 'react';
 
 export default class SendMessage extends Component {
+
+  onClick() {
+    console.log(this.refs.textarea.value);
+  }
+
   render () {
     return (
       <div className='form-group'>
         <label htmlFor="message">Message:</label>
-        <textarea className='form-control' rows="5"></textarea>
+        <textarea
+          className='form-control'
+          rows="5"
+          ref='textarea'>
+        </textarea>
 
-      <button type="button" className='btn btn-success btn-sm'> Send </button>
+        <button
+          type="button"
+          className='btn btn-success btn-sm'
+          onClick={this.onClick.bind(this)}> Send </button>
       </div>
     );
   }
